91f0d9c4b0f568cb1f06a40836365e1db43e672e
Titwin <titwin@live.fr>
2019-04-20 15:16:17 +0200

    import from src folder

--- Application.py
+++ Application.py
@@ -1,17 +1,29 @@
-import pyxel
+# permit application to import module from 'src' folder
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/src')
 
+# import modules
+import pyxel
+import InputManagerModule # as IMmodule
+
+# application class
 class App:
     def __init__(self):
         pyxel.init(160, 120)
         self.x = 0
+        self.im = InputManagerModule.InputManager()
+        
+        # has to be completely at the end of init
         pyxel.run(self.update, self.draw)
 
     def update(self):
         self.x = (self.x + 1) % pyxel.width
+        self.im.update()
 
     def draw(self):
         pyxel.cls(0)
         pyxel.rect(self.x, 0, self.x + 7, 7, 9)
 
-		
+# program entry
 App()
---
+++ src/InputManagerModule.py
@@ -0,0 +1,11 @@
+import pyxel
+
+
+class InputManager:
+    def __init__(self):
+        self.dummy = 0
+
+    def update(self):
+        self.dummy += 1
+        
+#InputManager()
