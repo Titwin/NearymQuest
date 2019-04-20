0384f885b59ce9aed39923f10ec447a2b5747441
Titwin <titwin@live.fr>
2019-04-20 17:46:19 +0200

    event system
    - add button event
    - add chord event (multiple button at a time)
    - add sequence event (a sequence of button press in a short time)

--- src/InputManagerModule.py
+++ src/InputManagerModule.py
@@ -1,11 +1,21 @@
 import pyxel
 
-
 class InputManager:
     def __init__(self):
-        self.dummy = 0
+        self.instantEventList = []
+
+    def addEvent(self, event):
+        self.instantEventList.append(event)
+
+    def removeEvent(self, event):
+        try:
+            self.instantEventList.remove(event)
+        except Exception as e:
+            print("InputManager : fail removing event")
 
     def update(self):
-        self.dummy += 1
+        frameEvent = []
+        for e in self.instantEventList:
+            if(e.update()):
+                frameEvent.append((e.notification, e.activated))
         
-#InputManager()
