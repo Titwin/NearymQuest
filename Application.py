fab06e1d7938493660bdd1d4743e17dd4ea106a8
solroo <solroo@gmail.com>
2019-04-20 20:03:24 +0200

    map render and walking

--- Application.py
+++ Application.py
@@ -7,12 +7,14 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/src')
 import pyxel
 import InputManagerModule # as IMmodule
 from Events import *
+from TilemapModule import *
 
 # application class
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
+        pyxel.init(255, 255)
         self.x = 0
+        self.y = 0
         self.im = InputManagerModule.InputManager()
 
         self.im.addEvent(Event(EventType.BUTTON, EventNotify.NONE, [pyxel.KEY_W], 'forward'))
@@ -24,17 +26,43 @@ class App:
 
         self.im.addEvent(Sequence([pyxel.KEY_W, pyxel.KEY_W], 'run'))
         
+        ## create the map
+        self.tilePalette = pyxel.image(0).load(0, 0, 'ressources/PathAndObjects.png')
+        self.map = Tilemap.ImportCSV("ressources/map1_path.csv",50,50)
+
         # has to be completely at the end of init
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        speedX = 3
+        speedY = 3
+        # handle character controller
         self.im.update()
+        if self.im.CheckEvent('forward'):
+            self.y = (self.y - 1*speedY) % pyxel.height
+        elif self.im.CheckEvent('backward'):
+            self.y = (self.y + 1*speedY) % pyxel.height
+        if self.im.CheckEvent('left'):
+            self.x = (self.x - 1*speedX) % pyxel.width    
+        elif self.im.CheckEvent('right'):
+            self.x = (self.x + 1*speedX) % pyxel.width
+
         #print(' ')
 
     def draw(self):
+        # clear the scene
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, self.x + 7, 7, 9)
+
+        TILE_SIZE = 32
+        # draw map
+        for y in range(self.map.sizeY):
+            for x in range(self.map.sizeX):
+                tile = self.map[(x,y)]
+                if tile._material != -1:
+                    pyxel.blt(x*TILE_SIZE, y*TILE_SIZE, self.tilePalette, tile._material%16*TILE_SIZE, tile._material//16*TILE_SIZE, TILE_SIZE, TILE_SIZE)
+
+        # handle character            
+        pyxel.rect(self.x, self.y, self.x + 16, self.y +32, 9)
 
 # program entry
 App()
--- src/InputManagerModule.py
+++ src/InputManagerModule.py
@@ -2,20 +2,40 @@ import pyxel
 
 class InputManager:
     def __init__(self):
-        self.instantEventList = []
+        self.EventList = []
 
     def addEvent(self, event):
-        self.instantEventList.append(event)
+        self.EventList.append(event)
 
     def removeEvent(self, event):
         try:
-            self.instantEventList.remove(event)
+            self.EventList.remove(event)
         except Exception as e:
             print("InputManager : fail removing event")
 
     def update(self):
-        frameEvent = []
-        for e in self.instantEventList:
+        self.frameEvent = []
+        for e in self.EventList:
             if(e.update()):
-                frameEvent.append((e.notification, e.activated))
+                self.frameEvent.append((e.notification, e.activated))
+
+    def CheckEventTriggerDown(self, eventName):
+        for e in self.frameEvent:
+            if(e[0].notification == eventName):
+                return e[1]
+        return False
+
+    def CheckEventTriggerUp(self, eventName):
+        for e in self.frameEvent:
+            if(e[0].notification == eventName):
+                return not e[1]
+        return False  
+
+    def CheckEvent(self, eventName):
+        for e in self.EventList:
+            if(e.notification == eventName):
+                return e.activated
+        return False
+
+
         
