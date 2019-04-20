0c90a617c4066c248fb01fbf8430033c0ff6ef11
Titwin <titwin@live.fr>
2019-04-20 21:03:05 +0200

    update controls

--- Application.py
+++ Application.py
@@ -22,9 +22,14 @@ class App:
         self.im.addEvent(Event(EventType.BUTTON, EventNotify.NONE, [pyxel.KEY_A], 'left'))
         self.im.addEvent(Event(EventType.BUTTON, EventNotify.NONE, [pyxel.KEY_D], 'right'))
 
-        self.im.addEvent(Event(EventType.CHORD, EventNotify.ALL, [pyxel.KEY_Q, pyxel.KEY_E], 'action1'))
+        self.im.addEvent(Sequence([pyxel.KEY_W, pyxel.KEY_W], 'dash forward'))
+        self.im.addEvent(Sequence([pyxel.KEY_S, pyxel.KEY_S], 'dash backward'))
+        self.im.addEvent(Sequence([pyxel.KEY_A, pyxel.KEY_A], 'dash left'))
+        self.im.addEvent(Sequence([pyxel.KEY_D, pyxel.KEY_D], 'dash right'))
 
-        self.im.addEvent(Sequence([pyxel.KEY_W, pyxel.KEY_W], 'run'))
+        self.im.addEvent(Event(EventType.BUTTON, EventNotify.NONE, [pyxel.KEY_SHIFT], 'run'))
+
+        self.dashTimer = 0
         
         ## create the map
         self.tilePalette = pyxel.image(0).load(0, 0, 'ressources/PathAndObjects.png')
@@ -34,10 +39,20 @@ class App:
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        self.im.update()
+
         speedX = 3
         speedY = 3
-        # handle character controller
-        self.im.update()
+        if self.im.CheckEvent('dash forward') or self.im.CheckEvent('dash backward') or self.im.CheckEvent('dash left') or self.im.CheckEvent('dash right'):
+            self.dashTimer = 10
+        if self.dashTimer > 0:
+            speedX *= 5
+            speedY *= 5
+            self.dashTimer -= 1
+        elif self.im.CheckEvent('run'):
+            speedX *= 2
+            speedY *= 2
+
         if self.im.CheckEvent('forward'):
             self.y = (self.y - 1*speedY) % pyxel.height
         elif self.im.CheckEvent('backward'):
@@ -47,19 +62,18 @@ class App:
         elif self.im.CheckEvent('right'):
             self.x = (self.x + 1*speedX) % pyxel.width
 
-        #print(' ')
 
     def draw(self):
         # clear the scene
         pyxel.cls(0)
 
-        TILE_SIZE = 32
-        # draw map
-        for y in range(self.map.sizeY):
-            for x in range(self.map.sizeX):
-                tile = self.map[(x,y)]
-                if tile._material != -1:
-                    pyxel.blt(x*TILE_SIZE, y*TILE_SIZE, self.tilePalette, tile._material%16*TILE_SIZE, tile._material//16*TILE_SIZE, TILE_SIZE, TILE_SIZE)
+        #TILE_SIZE = 32
+        ## draw map
+        #for y in range(self.map.sizeY):
+        #    for x in range(self.map.sizeX):
+        #        tile = self.map[(x,y)]
+        #        if tile._material != -1:
+        #            pyxel.blt(x*TILE_SIZE, y*TILE_SIZE, self.tilePalette, (tile._material%16)*TILE_SIZE, (tile._material//16)*TILE_SIZE, TILE_SIZE, TILE_SIZE)
 
         # handle character            
         pyxel.rect(self.x, self.y, self.x + 16, self.y +32, 9)
