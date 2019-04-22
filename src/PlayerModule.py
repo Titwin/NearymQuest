ad835f99ccfd839572f6197c25993427c7a46649
Titwin <titwin@live.fr>
2019-04-22 19:43:09 +0200

    character inheritance

--- src/PlayerModule.py
+++ src/PlayerModule.py
@@ -1,12 +1,11 @@
 from Events import *
 from TilemapModule import Tilemap
 from AnimationModule import *
-class Character:
+
+
+class Character(Entity):
     def __init__ (self):
-        self._x = 0
-        self._y = 0
-        self._w = 8
-        self._h = 16
+        Entity.__init__(self)
         self.dashTimer = 0
         self.speedX = 1
         self.speedY = 1
@@ -14,38 +13,6 @@ class Character:
         self.dy = 0
         self.orientationX = 1
         self.orientationY = 1
-
-    @property
-    def x(self):
-        return self._x
-
-    @x.setter
-    def x(self, new_x):
-        self._x = float(new_x)
-
-    @property
-    def y(self):
-        return self._y
-
-    @y.setter
-    def y(self, new_y):
-        self._y = float(new_y)
-
-    @property
-    def w(self):
-        return self._w
-
-    @w.setter
-    def w(self, new_w):
-        self._w = float(new_w)
-
-    @property
-    def h(self):
-        return self._h
-
-    @h.setter
-    def h(self, new_h):
-        self._h = float(new_h)
 
 
 class Player(Character):
