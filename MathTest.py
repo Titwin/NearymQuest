3a65d058a0509292cc5f78e522f8250769bd95e9
solroo <solroo@gmail.com>
2019-04-22 13:41:52 +0200

    Added Vector2i and Vector3i

--- MathTest.py
+++ MathTest.py
@@ -67,5 +67,67 @@ class Test:
 
         print("dot("+str(a)+","+str(a2)+") = "+ str(a.dot(a2)))
         print("cross("+str(a)+","+str(a2)+") = "+ str(a.cross(a2)))
+
+
+        a = Vector2i(2,3)
+        b = Vector2i(1,5)
+        c = 3
+        print("Vector2i operations")
+        print(""+str(a)+" + "+str(b)+" = "+ str(a+b))
+        print(""+str(a)+" + "+str(c)+" = "+ str(a+c))
+        print(""+str(c)+" + "+str(a)+" = "+ str(c+a))
+        
+        print(""+str(a)+" - "+str(b)+" = "+ str(a-b))
+        print(""+str(a)+" - "+str(c)+" = "+ str(a-c))
+        print(""+str(c)+" - "+str(a)+" = "+ str(c-a))
+        
+        print(""+str(a)+" * "+str(b)+" = "+ str(a*b))
+        print(""+str(a)+" * "+str(c)+" = "+ str(a*c))
+        print(""+str(c)+" * "+str(a)+" = "+ str(c*a))
+        
+        print(""+str(a)+" / "+str(b)+" = "+ str(a/b))
+        print(""+str(a)+" / "+str(c)+" = "+ str(a/c))
+        print(""+str(c)+" / "+str(a)+" = "+ str(c/a))
+
+        print(""+str(a)+" == "+str(b)+" = "+ str(a==b))
+        print(""+str(a)+" == "+str(a)+" = "+ str(a==a))
+        print(""+str(a)+" == "+str(c)+" = "+ str(a==c))
+        print(""+str(c)+" == "+str(a)+" = "+ str(c==a))
+        
+        print("dot("+str(a)+","+str(b)+") = "+ str(a.dot(b)))
+        print("----")
+        
+        print("Vector3i operations")
+        a = 0
+        b = 0
+
+        a = Vector3i(1,2,3)
+        a2 = Vector3i(2,1,3)
+        b = Vector2i(2,5)
+        c = 3
+        print(""+str(a)+" + "+str(b)+" = "+ str(a+b))
+        print(""+str(a)+" + "+str(c)+" = "+ str(a+c))
+        print(""+str(c)+" + "+str(a)+" = "+ str(c+a))
+        
+        print(""+str(a)+" - "+str(b)+" = "+ str(a-b))
+        print(""+str(a)+" - "+str(c)+" = "+ str(a-c))
+        print(""+str(c)+" - "+str(a)+" = "+ str(c-a))
+        
+        print(""+str(a)+" * "+str(b)+" = "+ str(a*b))
+        print(""+str(a)+" * "+str(c)+" = "+ str(a*c))
+        print(""+str(c)+" * "+str(a)+" = "+ str(c*a))
+        
+        print(""+str(a)+" / "+str(b)+" = "+ str(a/b))
+        print(""+str(a)+" / "+str(c)+" = "+ str(a/c))
+        print(""+str(c)+" / "+str(a)+" = "+ str(c/a))
+
+        print(""+str(a)+" == "+str(b)+" = "+ str(a==b))
+        print(""+str(a)+" == "+str(a)+" = "+ str(a==a))
+        print(""+str(a)+" == "+str(c)+" = "+ str(a==c))
+        print(""+str(c)+" == "+str(a)+" = "+ str(c==a))
+
+        print("dot("+str(a)+","+str(a2)+") = "+ str(a.dot(a2)))
+        print("cross("+str(a)+","+str(a2)+") = "+ str(a.cross(a2)))
+        
         
 Test()
--- src/Vector.py
+++ src/Vector.py
@@ -4,8 +4,8 @@ class Vector2f:
 
 # constructor
     def __init__(self, x=0, y=0): 
-        self._x = float(x) 
-        self._y = float(y)
+        self._x = self.__cast(x) 
+        self._y = self.__cast(y)
 
 # getters and setters 
     @property
@@ -14,7 +14,7 @@ class Vector2f:
 
     @x.setter
     def x(self, new_x):
-        self._x = float(new_x)
+        self._x = self.__cast(new_x)
 
     @property
     def y(self):
@@ -22,10 +22,13 @@ class Vector2f:
 
     @y.setter
     def y(self, new_y):
-        self._y = float(new_y)
+        self._y = self.__cast(new_y)
 
     def __str__(self):
-    	return "("+str(self._x)+","+str(self._y)+")"
+        return "("+str(self._x)+","+str(self._y)+")"
+
+    def __cast(self,component):
+        return float(component)
 
 # Binary Operators
     ### +
@@ -135,9 +138,9 @@ class Vector3f:
 
 # constructor
     def __init__(self, x=0, y=0, z=0): 
-        self._x = float(x) 
-        self._y = float(y) 
-        self._z = float(z) 
+        self._x = self.__cast(x) 
+        self._y = self.__cast(y) 
+        self._z = self.__cast(z) 
 
 # getters and setters 
     @property
@@ -146,7 +149,7 @@ class Vector3f:
 
     @x.setter
     def x(self, new_x):
-        self._x = float(new_x)
+        self._x = self.__cast(new_x)
 
     @property
     def y(self):
@@ -154,7 +157,7 @@ class Vector3f:
 
     @y.setter
     def y(self, new_y):
-        self._y = float(new_y)
+        self._y = self.__cast(new_y)
 
     @property
     def z(self):
@@ -162,11 +165,14 @@ class Vector3f:
 
     @z.setter
     def z(self, new_z):
-        self._z = float(new_z)
+        self._z = self.__cast(new_z)
 
     def __str__(self):
-    	return "("+str(self._x)+","+str(self._y)+","+str(self._z)+")"
+        return "("+str(self._x)+","+str(self._y)+","+str(self._z)+")"
 
+    def __cast(self,component):
+        return float(component)
+        
 # Binary Operators
     ### +
     def __add__(self, other): 
@@ -286,4 +292,301 @@ class Vector3f:
     @property
     def one(self):
         return Vector3f(1,1,1)
+
+
+
+  
+class Vector2i:
+
+# constructor
+    def __init__(self, x=0, y=0): 
+        self._x = self.__cast(x) 
+        self._y = self.__cast(y)
+
+# getters and setters 
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, new_x):
+        self._x = self.__cast(new_x)
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, new_y):
+        self._y = self.__cast(new_y)
+
+    def __str__(self):
+        return "("+str(self._x)+","+str(self._y)+")"
+
+    def __cast(self,component):
+        return int(component)
+
+# Binary Operators
+    ### +
+    def __add__(self, other): 
+        if isinstance(other, Vector2i):
+            return Vector2i(self.x+other.x,self.y+other.y)
+        elif isinstance(other, (int,float)):
+            return Vector2i(self.x+other,self.y+other)
+    def __radd__(self, other): 
+       return Vector2i.__add__(self,other)
+
+    ### -
+    def __sub__(self, other):
+        if isinstance(other, Vector2i):
+            return Vector2i(self.x-other.x,self.y-other.y)
+        elif isinstance(other, (int,float)):
+            return Vector2i(self.x-other,self.y-other)
+    def __rsub__(self, other): 
+       return Vector2i.__add__(-self,other)
+       
+    ### *
+    def __mul__(self, other):
+        if isinstance(other, Vector2i):
+            return Vector2i(self.x*other.x,self.y*other.y)
+        elif isinstance(other, (int,float)):
+            return Vector2i(self.x*other,self.y*other)
+    def __rmul__(self, other):
+        return Vector2i.__mul__(self,other)
+
+    ### /
+    def __truediv__(self, other):
+        if isinstance(other, Vector2i):
+            return Vector2i(self.x/other.x,self.y/other.y)
+        elif isinstance(other, (int,float)):
+            return Vector2i(self.x/other,self.y/other)
+    def __rtruediv__(self, other):
+        if isinstance(other, Vector2i):
+            return Vector2i(other.x/self.x,other.y/self.y)
+        elif isinstance(other, (int,float)):
+            return Vector2i(other/self.x,other/self.y)
+    ### //
+    def __floordiv__(self, other):
+        if isinstance(other, Vector2i):
+            return Vector2i(self.x//other.x,self.y//other.y)
+        elif isinstance(other, (int,float)):
+            return Vector2i(self.x//other,self.y//other)
+    ### %
+    def __mod__(self, other):
+        if isinstance(other, Vector2i):
+            return Vector2i(self.x/other.x,self.y/other.y)
+        elif isinstance(other, (int,float)):
+            return Vector2i(self.x/other,self.y/other)
+    ### **
+    def __pow__(self, other):
+        if isinstance(other, Vector2i):
+            return Vector2i(self.x**other.x,self.y**other.y)
+        elif isinstance(other, (int,float)):
+            return Vector2i(self.x**other,self.y**other)
+
+    ### ==
+    def __eq__(self, other):
+        return isinstance(other, Vector2i) and self.x == other.x and self.y == other.y
+
+    def dot(self, other):
+        if isinstance(other, Vector2i):
+            return self.x*other.x+self.y*other.y
+
+# Unary Operators    
+    def __neg__(self):
+        return Vector2i(-self.x, -self.y)
+
+    @property    
+    def magnitude(self):
+        return math.sqrt(self.magnitudeSqr)
+    @property
+    def magnitudeSqr(self):
+        return self.x*self.x+self.y*self.y
+
+    @property
+    def normalized(self):
+        mag = self.magnitude()
+        if mag == 0 :
+            raise Exception('Magnitude is zero')
+        else:
+            self.x /= mag
+            self.y /= mag
+
+    def normalize(self):
+        mag = self.magnitude()
+        if mag == 0 :
+            raise Exception('Magnitude is zero')
+        else:
+            return Vector2f(self.x/mag,self.y/mag)
+
+# Commodity constructors
+    @property
+    def zero(self):
+        return Vector2i(0,0)
+
+    @property
+    def one(self):
+        return Vector2i(1,1)
         
+        
+
+class Vector3i:
+
+# constructor
+    def __init__(self, x=0, y=0, z=0): 
+        self._x = self.__cast(x) 
+        self._y = self.__cast(y) 
+        self._z = self.__cast(z) 
+
+# getters and setters 
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, new_x):
+        self._x = self.__cast(new_x)
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, new_y):
+        self._y = self.__cast(new_y)
+
+    @property
+    def z(self):
+        return self._z
+
+    @z.setter
+    def z(self, new_z):
+        self._z = self.__cast(new_z)
+
+    def __str__(self):
+        return "("+str(self._x)+","+str(self._y)+","+str(self._z)+")"
+
+    def __cast(self,component):
+        return int(component)
+
+# Binary Operators
+    ### +
+    def __add__(self, other): 
+        if isinstance(other, Vector3i):
+            return Vector3i(self.x+other.x,self.y+other.y,self.z+other.z)
+        elif isinstance(other, (int,float)):
+            return Vector3i(self.x+other,self.y+other,self.z+other)
+        elif isinstance(other, Vector2i):
+            return Vector3i(self.x+other.x,self.y+other.y,self.z)
+    def __radd__(self, other): 
+       return Vector3i.__add__(self,other)
+
+    ### -
+    def __sub__(self, other):
+        if isinstance(other, Vector3i):
+            return Vector3i(self.x-other.x,self.y-other.y,self.z-other.z)
+        elif isinstance(other, (int,float)):
+            return Vector3i(self.x-other,self.y-other,self.z-other)
+        elif isinstance(other, Vector2i):
+            return Vector3i(self.x-other.x,self.y-other.y,self.z)
+    def __rsub__(self, other): 
+       if isinstance(other, Vector3i):
+           return Vector3i(-self.x+other.x,-self.y+other.y,-self.z+other.z)
+       elif isinstance(other, (int,float)):
+           return Vector3i(-self.x+other,-self.y+other,-self.z+other)
+       elif isinstance(other, Vector2i):
+            return Vector3i(-self.x+other.x,-self.y+other.y,-self.z)
+       
+    ### *
+    def __mul__(self, other):
+        if isinstance(other, Vector3i):
+            return Vector3i(self.x*other.x,self.y*other.y,self.z*other.z)
+        elif isinstance(other, (int,float)):
+            return Vector3i(self.x*other,self.y*other,self.z*other)
+        elif isinstance(other, Vector2i):
+            return Vector3i(self.x*other.x,self.y*other.y,self.z)
+    def __rmul__(self, other):
+        return Vector3i.__mul__(self,other)
+
+    ### /
+    def __truediv__(self, other):
+        if isinstance(other, Vector3i):
+            return Vector3i(self.x/other.x,self.y/other.y,self.z/other.z)
+        elif isinstance(other, (int,float)):
+            return Vector3i(self.x/other,self.y/other,self.z/other)
+    def __rtruediv__(self, other):
+        if isinstance(other, Vector3i):
+            return Vector3i(other.x/self.x,other.y/self.y,other.z/self.z)
+        elif isinstance(other, (int,float)):
+            return Vector3i(other/self.x,other/self.y,other/self.z)
+    ### //
+    def __floordiv__(self, other):
+        if isinstance(other, Vector3i):
+            return Vector3i(self.x//other.x,self.y//other.y,self.z//other.z)
+        elif isinstance(other, (int,float)):
+            return Vector3i(self.x//other,self.y//other,self.z//other)
+    ### %
+    def __mod__(self, other):
+        if isinstance(other, Vector3i):
+            return Vector3i(self.x/other.x,self.y/other.y,self.z/other.z)
+        elif isinstance(other, (int,float)):
+            return Vector3i(self.x/other,self.y/other,self.z/other)
+    ### **
+    def __pow__(self, other):
+        if isinstance(other, Vector3i):
+            return Vector3i(self.x**other.x,self.y**other.y,self.z**other.z)
+        elif isinstance(other, (int,float)):
+            return Vector3i(self.x**other,self.y**other,self.z**other)
+
+    ### ==
+    def __eq__(self, other):
+        return isinstance(other, Vector3i) and self.x == other.x and self.y == other.y and self.z == other.z
+
+    def dot(self, other):
+        if isinstance(other, Vector3i):
+            return self.x*other.x+self.y*other.y+self.z*other.z
+    
+    def cross(self, other):
+        if isinstance(other, Vector3i):
+            return Vector3i(self.y*other.z - self.z*other.y,
+                            self.z*other.x - self.x*other.z,
+                            self.x*other.y - self.y*other.x)
+
+# Unary Operators    
+    def __neg__(self):
+        return Vector3i(-self.x, -self.y, -self.z)
+
+    @property    
+    def magnitude(self):
+        return math.sqrt(self.magnitudeSqr)
+    @property
+    def magnitudeSqr(self):
+        return self.x*self.x+self.y*self.y+self.z*self.z
+
+    @property
+    def normalized(self):
+        mag = self.magnitude()
+        if mag == 0 :
+            raise Exception('Magnitude is zero')
+        else:
+            self.x /= mag
+            self.y /= mag
+            self.z /= mag
+
+    def normalize(self):
+        mag = self.magnitude()
+        if mag == 0 :
+            raise Exception('Magnitude is zero')
+        else:
+            return Vector3f(self.x/mag,self.y/mag,self.z/mag)
+
+# Commodity constructors
+    @property
+    def zero(self):
+        return Vector3i(0,0,0)
+
+    @property
+    def one(self):
+        return Vector3i(1,1,1)
+        
+
